6b97c803a684565cc1f4d9426896ad1e3ea9c671
hannahmariamoran <143641022+hannahmariamoran@users.noreply.github.com>
2024-05-07 17:32:57 -0400

    working on landing page

---
+++ main.js
@@ -0,0 +1,30 @@
+const renderItems = (data) => {
+    // The `ul` where the timeline items will be inserted
+    const dataList = document.getElementById('project-about');
+
+    // Loop through each item in the data array
+    data.forEach((item) => {
+        // Make a template literal as we have before, inserting your data
+        let Item = `
+            <div> 
+                <h2>${item.Title}<h2>
+                <h3>${item.Subtitle}<h3>
+                <p>${item.Description}<p>
+            </div>
+        `;
+
+        dataList.insertAdjacentHTML('beforeend', Item); 
+    });
+
+};
+
+// Fetch gets your (local) JSON file…
+fetch('js/data.json')
+    .then(response => response.json())
+    .then(data => {
+        // And passes the data to the function, above!
+        renderItems(data);
+    })
+    .catch(error => {
+        console.error('Error fetching JSON data:', error);
+    });
